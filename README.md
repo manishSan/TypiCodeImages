fea49ce892893c788d505d945060846348c9c338
manish <manish.sanwal@gmail.com>
2019-03-18 23:28:41 -0400

    Added ApiClient.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TypiCodeImages
 An iOS app challenge to display a list of images download from http://jsonplaceholder.typicode.com/photos.
+
+
+Use HTTPS - 1. Arbitrary load = yes
+
+Separation of concerns
+
+Mutability
+
+One way data flow
+
+protocol oriented programming
+
+Dependency Injection
+
+
+
+Open Source used
--- TypiCodeImages/ApiClient/ApiClient.swift
+++ TypiCodeImages/ApiClient/ApiClient.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import RxSwift
+
+/// API Client specification
+protocol ApiClientProtocol {
+
+    func images() -> Single<Result<[Image]>>
+}
+
+/// ApiClient concrete implementation
+struct ApiClient: ApiClientProtocol {
+
+    let networkProvider: NetworkProtocol
+
+    func images() -> Single<Result<[Image]>> {
+        return networkProvider
+            .fetchImages().map({ (result) -> Result<[Image]> in
+                switch result {
+                case .success(let data):
+                    do {
+                        let images = try JSONDecoder().decode([Image].self, from: data)
+                        return Result.success(images)
+                    } catch {
+                        return Result.failure(AppError.errorParsingData.error())
+                    }
+                case .failure(let error):
+                    return Result.failure(error)
+                }
+            })
+    }
+}
